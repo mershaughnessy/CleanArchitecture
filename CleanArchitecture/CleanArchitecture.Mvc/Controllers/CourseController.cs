294929018568cf0a9f74f4b0ca45d7a922c19d80
Maximillian Edward Shaughnessy <78328737+mershaughnessy@users.noreply.github.com>
2022-01-06 14:37:18 +0000

    Commit changes for presentation course controller.

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
--- CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
+++ CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
@@ -59,6 +59,7 @@ namespace CleanArchitecture.Mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
