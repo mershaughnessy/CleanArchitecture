4aee3e45143e56426e013e4926fd24c593d5f06b
Maximillian Edward Shaughnessy <78328737+mershaughnessy@users.noreply.github.com>
2022-01-06 10:11:16 +0000

    Commit implementation of service and Repository methods.

--- CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
+++ CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -7,6 +7,6 @@ namespace CleanArchitecture.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<CourseViewModel> GetCourses();
+        CourseViewModel GetCourses();
     }
 }
